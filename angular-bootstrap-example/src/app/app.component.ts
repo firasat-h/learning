ce3b5bfc46d71c75e9195234edff5ab7ce027558
Firasat Hussain <rr175820@IL0086AM071100.na.ad.rrd.com>
2018-01-15 21:37:56 -0600

    first commit

---
+++ angular-bootstrap-example/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component, TemplateRef} from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  public modalRef: BsModalRef; // {1}
+  constructor(private modalService: BsModalService) {} // {2}
+
+  public openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template); // {3}
+  }
+}
---
+++ spring-rest-service/src/main/java/employee/Employee.java
@@ -0,0 +1,26 @@
+package employee;
+
+public class Employee {
+    private String name;
+    private String city;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
